68c98dc8819344ca383d5223ed6666b82a7d2ffe
Fannichan <elekes.fanni2005@gmail.com>
2024-03-08 11:13:39 +0100

    page login

---
+++ Allergens.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vizsgaremek_Asztali
+{
+    class Allergens
+    {
+        private int id;
+        private string name;
+    }
+}
